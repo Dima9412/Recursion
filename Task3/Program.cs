3415ac8d65aa30d7d903487a57ffe655e82b7abe
Dima9412 <165942648+Dima9412@users.noreply.github.com>
2024-06-04 23:18:11 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = {1, 2, 5, 0, 10, 34};
+        Console.WriteLine(String.Join(", ", ReversePrint(array)));
+    }
+
+    private static string ReversePrint(int[] array)
+    {
+        if (array.Length > 0)
+        {
+            return $"{array[array.Length - 1]}, {ReversePrint(array.Take(array.Length - 1).ToArray())}";
+        }
+        else
+        {
+            return "";
+        }
+    }
+}
