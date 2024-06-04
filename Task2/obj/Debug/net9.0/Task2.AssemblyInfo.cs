b8e88a949e4e516329f2af5b3a0732798a2d9c05
Dima9412 <165942648+Dima9412@users.noreply.github.com>
2024-06-04 23:18:34 +0300

    Task2/

--- Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d36feaf114594b352c4a3fcdf8b032c4c9aac9c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e680a8657de4d58d169efbbb1b1467a1b8cf6b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
