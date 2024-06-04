ba0e128c1c24d0526f6a6b5a09d7f37a80e25394
Dima9412 <165942648+Dima9412@users.noreply.github.com>
2024-06-04 22:22:38 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите начальное число: ");
+        int m = int.Parse(Console.ReadLine());
+        Console.Write("Введите конечное число: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNumbers(m, n);
+    }
+
+    private static void PrintNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.Write(m + ",");
+            m++;
+            PrintNumbers(m, n);
+        }
+    }
+}
+
